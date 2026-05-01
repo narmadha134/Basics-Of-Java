1744c6e55144686b8de2b43b1f8dfd7f37b0c2ff
narmadha134 <krnarmadhaofficial03@gmail.com>
2026-05-01 12:28:08 +0530

    Basics Of Java
    Signed-off-by: narmadha134 <krnarmadhaofficial03@gmail.com>

---
+++ task1.java
@@ -0,0 +1,11 @@
+class task1 {
+    public static void main(String[] args) {
+        char m = 'a';
+
+        if (m == 'a' || m == 'e' || m == 'i' || m == 'o' || m == 'u') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
+    }
+}
